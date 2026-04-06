45ad9c74c3ec88f89574b9c174e0f93eb8a59ba0
Lae Lae Tun <laelaetun17116@gmail.com>
2026-04-06 20:21:56 +0630

    refactor: clean up DataProductModel constructor formatting and remove redundant whitespace

--- lib/product/data/model/data_product_model.dart
+++ lib/product/data/model/data_product_model.dart
@@ -1,13 +1,10 @@
 import 'package:product/product/domain/entities/product_entity.dart';
 
 class DataProductModel extends ProductEntity {
-  DataProductModel({
-    required super.id,
-    required super.title,
-  });
+  DataProductModel({required super.id, required super.title});
 
-  factory DataProductModel.fromJson(Map<String,dynamic> json){
+  factory DataProductModel.fromJson(Map<String, dynamic> json) {
     return DataProductModel(id: json['id'], title: json['title']);
   }
-  
+  //jjjk
 }
